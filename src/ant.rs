e5ac4921be24452c248b41a1e0e9ab14236b55d1
Hans Larsson <hanandlia@gmail.com>
2024-05-24 11:35:07 -0500

    create an ant bundle and defaults for it

--- src/ant.rs
+++ src/ant.rs
@@ -1,10 +1,36 @@
-use bevy::prelude::*;
+use bevy::{math::vec3, prelude::*};
 
 #[derive(Component)]
 pub struct AntPlugin;
 
 #[derive(Component)]
-pub struct Ant;
+pub enum AntRole {
+    Egg,
+    Grub,
+    Adult,
+    Queen,
+}
+
+#[derive(Bundle)]
+struct AntBundle {
+    sprite: SpriteBundle,
+    ant_role: AntRole,
+}
+
+impl Default for AntBundle {
+    fn default() -> Self {
+        Self {
+            sprite: SpriteBundle {
+                transform: Transform {
+                    scale: vec3(10.0, 10.0, 0.0),
+                    ..default()
+                },
+                ..default()
+            },
+            ant_role: AntRole::Adult,
+        }
+    }
+}
 
 impl Plugin for AntPlugin {
     fn build(&self, app: &mut App) {
@@ -14,7 +40,5 @@ impl Plugin for AntPlugin {
 
 fn spawn_ants(mut commands: Commands) {
     info!("AntPlugin loaded!");
-    commands.spawn((
-        SpriteBundle {..default()},
-        Ant));
+    commands.spawn(AntBundle { ..default() });
 }
