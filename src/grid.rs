7bf08edb85263b8d1f4d967dddf9a3abe3dd266b
Hans Larsson <hanandlia@gmail.com>
2025-03-03 10:17:25 -0600

    add documentation for grid.rs

--- src/grid.rs
+++ src/grid.rs
@@ -8,6 +8,7 @@ impl Plugin for GridPlugin {
 
 pub struct GridPlugin;
 
+// TheGrid is used for trakcing the grid map that ants move on and dirt is placed on
 #[derive(Resource)]
 pub struct TheGrid {
     pub grid: Vec<GridCell>,
@@ -15,18 +16,22 @@ pub struct TheGrid {
     pub width: usize,
 }
 
+// A GridCell is used to track a location and draw a debug sprite onto that location
 #[derive(Bundle)]
 pub struct GridCell {
     location: GridLocation,
     sprite: SpriteBundle,
 }
 
+// A GridLocation is used as cordinates for the map at large
 #[derive(Component)]
 pub struct GridLocation {
     pub x: usize,
     pub y: usize,
 }
 
+// Sets up a default 5x5 grid
+// TODO: make this fill the whole window with a grid
 impl Default for TheGrid {
     fn default() -> Self {
         let (height, width) = (5, 5);
@@ -38,6 +43,7 @@ impl Default for TheGrid {
     }
 }
 
+// initializes the grid for the world
 fn make_grid(mut commands: Commands) {
     commands.init_resource::<TheGrid>();
 }
